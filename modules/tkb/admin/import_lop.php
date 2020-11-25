e89476e5fa05675f01e38140afdb7343ce9c7cdb
Dat Huynh <dat.huynh@infodation.vn>
2020-11-25 16:49:51 +0700

    lọc buổi khi import TKB mới

--- modules/tkb/admin/import_lop.php
+++ modules/tkb/admin/import_lop.php
@@ -1,7 +1,7 @@
 <?php
 
-if (! defined('NV_IS_TKB_ADMIN')) {
-    die('Stop!!!');
+if (!defined('NV_IS_TKB_ADMIN')) {
+	die('Stop!!!');
 }
 
 include dirname(__FILE__) . '/../plugins/PHPExcel/Classes/PHPExcel/IOFactory.php';
@@ -17,33 +17,33 @@ if ($nv_Request->isset_request('do', 'post')) {
 
 	if (isset($_FILES['ufile']) && is_uploaded_file($_FILES['ufile']['tmp_name'])) {
 		$filename = nv_string_to_filename($_FILES['ufile']['name']);
-        $file = NV_ROOTDIR . '/' . NV_TEMP_DIR . '/' . $filename;
-        if(file_exists($file)){
-            unlink($file);
-        }
-        if (move_uploaded_file($_FILES['ufile']['tmp_name'], $file)) {
+		$file = NV_ROOTDIR . '/' . NV_TEMP_DIR . '/' . $filename;
+		if (file_exists($file)) {
+			unlink($file);
+		}
+		if (move_uploaded_file($_FILES['ufile']['tmp_name'], $file)) {
 			if (file_exists($file)) {
-	            try {
-	                $fileType = PHPExcel_IOFactory::identify($file);
-	                $objReader = PHPExcel_IOFactory::createReader($fileType);
-	                $objPHPExcel = $objReader->load($file);
-	            } catch(Exception $e) {
-                 	$error = $lang_module['error_cannot_read_file'].' '.pathinfo($file,PATHINFO_BASENAME).'": '.$e->getMessage();
-	            }
+				try {
+					$fileType = PHPExcel_IOFactory::identify($file);
+					$objReader = PHPExcel_IOFactory::createReader($fileType);
+					$objPHPExcel = $objReader->load($file);
+				} catch (Exception $e) {
+					$error = $lang_module['error_cannot_read_file'] . ' ' . pathinfo($file, PATHINFO_BASENAME) . '": ' . $e->getMessage();
+				}
 
-	            if (empty($error)) {
-	            	$sheet = $objPHPExcel->getSheet(0); 
-		            $highestRow = $sheet->getHighestRow();
-		            $highestColumn = $sheet->getHighestColumn();
-		             
-		            $data = [];
-		            for ($row = 1; $row <= $highestRow; $row++){
-		                $data[] = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE);
-		            }
-		            
-		            //_array_shift($data, 2);
+				if (empty($error)) {
+					$sheet = $objPHPExcel->getSheet(0);
+					$highestRow = $sheet->getHighestRow();
+					$highestColumn = $sheet->getHighestColumn();
 
-		            // Bắt đầu tạo dữ liệu để add vào database
+					$data = [];
+					for ($row = 1; $row <= $highestRow; $row++) {
+						$data[] = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE);
+					}
+
+					//_array_shift($data, 2);
+
+					// Bắt đầu tạo dữ liệu để add vào database
 					$list_lop = $data[0][0];
 					_array_shift($list_lop, 2);
 
@@ -63,7 +63,7 @@ if ($nv_Request->isset_request('do', 'post')) {
 						}
 
 						$tiet++;
-						if($tiet == 6) {
+						if ($tiet == 6) {
 							$tiet = 1;
 							$thu++;
 						}
@@ -71,32 +71,32 @@ if ($nv_Request->isset_request('do', 'post')) {
 
 					// Bắt đầu import vào database
 					foreach ($data_import as $_lop => $val1) {
-						$db->query("DELETE FROM " . NV_PREFIXLANG . "_" . $module_data . "_lop WHERE lop = '".$_lop."'");
+						$db->query("DELETE FROM " . NV_PREFIXLANG . "_" . $module_data . "_lop WHERE lop = '" . $_lop . "' AND buoi = " . $buoi);
 						foreach ($val1 as $_tiet => $_mon) {
 							$_sql = 'INSERT INTO ' . NV_PREFIXLANG . '_' . $module_data . '_lop
 								(lop, buoi, tiet, thu2, thu3, thu4, thu5, thu6, thu7) VALUES
 								(:lop, :buoi, :tiet, :thu2, :thu3, :thu4, :thu5, :thu6, :thu7)';
-							
+
 							$sth = $db->prepare($_sql);
-		            		$sth->bindParam(':lop', $_lop, PDO::PARAM_STR);
-		            		$sth->bindParam(':tiet', $_tiet, PDO::PARAM_INT);
-		            		$sth->bindParam(':buoi', $buoi, PDO::PARAM_INT);
-		            		
+							$sth->bindParam(':lop', $_lop, PDO::PARAM_STR);
+							$sth->bindParam(':tiet', $_tiet, PDO::PARAM_INT);
+							$sth->bindParam(':buoi', $buoi, PDO::PARAM_INT);
+
 							foreach ($_mon as $_thu => $_nocare) {
-								$sth->bindParam(':thu'.$_thu, $_mon[$_thu], PDO::PARAM_STR);
+								$sth->bindParam(':thu' . $_thu, $_mon[$_thu], PDO::PARAM_STR);
 							}
 							$sth->execute();
 						}
 					}
 					$success = $lang_module['import_success'];
 					unlink($file);
-	            }
-	        } else {
-	        	$error = $lang_module['error_upload_file'];
-	        }
+				}
+			} else {
+				$error = $lang_module['error_upload_file'];
+			}
 		} else {
-        	$error = $lang_module['error_upload_file'];
-        }
+			$error = $lang_module['error_upload_file'];
+		}
 	} else {
 		$error = $lang_module['error_dont_have_file'];
 	}
@@ -122,7 +122,7 @@ if ($success) {
 
 $xtpl->parse('main');
 $contents = $xtpl->text('main');
- 
-include (NV_ROOTDIR . "/includes/header.php");
+
+include(NV_ROOTDIR . "/includes/header.php");
 echo nv_admin_theme($contents);
-include (NV_ROOTDIR . "/includes/footer.php");
+include(NV_ROOTDIR . "/includes/footer.php");
